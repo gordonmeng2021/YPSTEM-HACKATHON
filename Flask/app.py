586c39d92c159ffda0bf466c045afa018024e2b8
meng <gordonmeng2021@gmail.com>
2022-02-28 15:21:27 +0800

    homepage update

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,6 @@
 # Entry point for the application.
 from . import app    # For application discovery by the 'flask' command.
-from . import views  # For import side-effects of setting up routes.
+from . import views # For import side-effects of setting up routes.
 
 #To run the file:
 #Type "set FLASK_APP=portfolio.webapp" in terminal.
